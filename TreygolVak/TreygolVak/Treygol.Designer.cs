fc6092e8da1edf4c567b34cdc27eb1c14b993f08
NankainaPoro <113505829+NankainaPoro@users.noreply.github.com>
2023-10-16 20:18:21 +0300

    Add files via upload

---
+++ TreygolVak/TreygolVak/Treygol.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace TreygolVak
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LenA = new System.Windows.Forms.TextBox();
+            this.LenB = new System.Windows.Forms.TextBox();
+            this.LenC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Result = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LenA
+            // 
+            this.LenA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LenA.Location = new System.Drawing.Point(108, 193);
+            this.LenA.Multiline = true;
+            this.LenA.Name = "LenA";
+            this.LenA.Size = new System.Drawing.Size(153, 35);
+            this.LenA.TabIndex = 0;
+            this.LenA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LenA.TextChanged += new System.EventHandler(this.LenA_TextChanged);
+            // 
+            // LenB
+            // 
+            this.LenB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LenB.Location = new System.Drawing.Point(328, 193);
+            this.LenB.Multiline = true;
+            this.LenB.Name = "LenB";
+            this.LenB.Size = new System.Drawing.Size(153, 35);
+            this.LenB.TabIndex = 1;
+            this.LenB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LenB.TextChanged += new System.EventHandler(this.LenB_TextChanged);
+            // 
+            // LenC
+            // 
+            this.LenC.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LenC.Location = new System.Drawing.Point(543, 193);
+            this.LenC.Multiline = true;
+            this.LenC.Name = "LenC";
+            this.LenC.Size = new System.Drawing.Size(153, 37);
+            this.LenC.TabIndex = 2;
+            this.LenC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LenC.TextChanged += new System.EventHandler(this.LenC_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(101, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(595, 42);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите стороны треугольника";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(173, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "a";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(392, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "b";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(608, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "c";
+            // 
+            // Result
+            // 
+            this.Result.AutoSize = true;
+            this.Result.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result.Location = new System.Drawing.Point(276, 10);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(170, 39);
+            this.Result.TabIndex = 8;
+            this.Result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(454, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 41);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Result, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 356);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 94);
+            this.tableLayoutPanel1.TabIndex = 10;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LenC);
+            this.Controls.Add(this.LenB);
+            this.Controls.Add(this.LenA);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LenA;
+        private System.Windows.Forms.TextBox LenB;
+        private System.Windows.Forms.TextBox LenC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label Result;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ TreygolVak/TreygolVak/Treygol.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TreygolVak
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (LenA.Text == "" || LenB.Text == "" || LenC.Text == "")
+            {
+                Result.Text = "Введите стороны!";
+                Result.Visible = true;
+                return;
+            }
+            // else if (LenA.Text == || LenB.Text == "" || LenC.Text == "")
+            //{ }
+
+            try
+            {
+                Convert.ToDouble(LenA.Text);
+                Convert.ToDouble(LenB.Text);
+                Convert.ToDouble(LenC.Text);
+            }
+            catch
+            {
+                Result.Text = "Введите стороны корректно!";
+                Result.Visible = true;
+                return;
+            }
+
+            double a = Convert.ToDouble(LenA.Text);
+            double b = Convert.ToDouble(LenB.Text);
+            double c = Convert.ToDouble(LenC.Text);
+            double d = a;
+
+
+            if (b > d)
+                d = b;
+            if (c > d)
+                d = c;
+
+
+            if (a + b <= d || b + c <= d || a + c <= d)
+            {
+                Result.Text = ("Треугольника не существует");
+            }
+            else if (a == b && b == c)
+            {
+                Result.Text = "Треугольник равносторонний";
+            }
+            else if (a == b || a == c || b == c)
+            {
+                Result.Text = "Треугольник равнобедренный";
+            }
+            else
+            {
+                Result.Text = ("Треугольник разносторонний");
+            }
+
+            Result.Visible = true;
+
+
+
+
+
+
+
+        }
+
+        private void LenA_TextChanged(object sender, EventArgs e)
+        {
+            //if (LenA.Text.Length == 0 || LenB.Text.Length == 0 || LenC.Text.Length == 0)
+            //{
+            //    button1.Enabled = false;
+            //}
+            //else button1.Enabled = true;
+        }
+
+        private void LenB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LenC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LenA.Clear();
+            LenB.Clear();
+            LenC.Clear();
+            Result.Text = "";
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
